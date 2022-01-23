e124551a26fcd4784e76db5cf3af0269fcc86cf1
juanmedcas <juanmedina10013@gmail.com>
2022-01-23 01:04:38 -0500

    Seccion 4 del Curso, se agrega readme y .ignore, distintos ejercicios de las lecciones.

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-//console.log( 'Hola Mundo' );
-//Fahrenheit a CELCIUS
-let c, k;
-let f = 90;
-c = (f-32)*(5/9);
-console.log('Estos son grados Fahrenheit a Celcius: '+c);
+/*
+    Cree un algoritmo que permita determinar 
+    la calificación en letras de un alumno
+
+    Si la nota se encuentra entre 100 a 90 = A
+    Si la nota se encuentra entre 80 a 89 = B
+    Si la nota se encuentra entre 70 a 79 = C
+    Si la nota se encuentra entre 60 a 69 = D
+    Si la nota se inferior a 60 = F
+
+*/
+
+let nota = -10;
+let notaLetra = '';
+
+if (nota >= 0 && nota <= 100) {
+    if (nota >= 90 && nota <= 100) {
+        notaLetra = 'A';
+    } else if (nota >= 80 && nota <= 89) {
+        notaLetra = 'B';
+    } else if (nota >= 70 && nota <= 79) {
+        notaLetra = 'C';
+    } else if (nota >= 60 && nota <= 69) {
+        notaLetra = 'D';
+    } else if (nota >= 0 && nota <= 59) {
+        notaLetra = 'F';
+    }
+    console.log(nota + ' es igual a ' + notaLetra );
+
+} else if (nota >= 101 ) {
+    console.log('La nota esta fuera de Rango!');
+} else {
+    console.log('La nota esta fuera de Rango!!');
+}
 
 
-//Fahrenheit a KELVIN
-k = (f-32)*5/9+273.15;
-console.log('Estos son grados Fahrenheit a Kelvin: '+k);
 
 
-//Celcius a Fahrenheit
-c = 50;
-f = 0;
-f = (c*9/5)+32;
-console.log('Estos son grados Celcius a Fahrenheit: '+f);
-
-
-//Celcius a Kelvin
-k = 0;
-c = 0;
-k = c+273.15;
-console.log('Estos son grados Celcius a KELVIN: '+k);
